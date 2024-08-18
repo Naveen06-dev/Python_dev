023fa0da9306fde757450afbb091ebbed28fc962
Naveen06-dev <naveenkvg8@gmail.com>
2024-08-18 16:05:05 +0530

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -3,53 +3,43 @@ import os
 import base64
 import json
 
-# Generate a key using a password
 def generate_key(password):
     return base64.urlsafe_b64encode(password.ljust(32)[:32].encode())
 
-# Encrypt a message
 def encrypt_message(message, key):
     fernet = Fernet(key)
     return fernet.encrypt(message.encode()).decode()
 
-# Decrypt a message
 def decrypt_message(encrypted_message, key):
     fernet = Fernet(key)
     return fernet.decrypt(encrypted_message.encode()).decode()
 
-# Save passwords to a file
 def save_passwords(filename, passwords):
     with open(filename, 'w') as f:
         json.dump(passwords, f)
 
-# Load passwords from a file
 def load_passwords(filename):
     if not os.path.exists(filename):
         return {}
     with open(filename, 'r') as f:
         return json.load(f)
-
-# Add or update a password
+d
 def add_password(filename, key, service, password):
     passwords = load_passwords(filename)
     passwords[service] = encrypt_message(password, key)
     save_passwords(filename, passwords)
 
-# Get a password
 def get_password(filename, key, service):
     passwords = load_passwords(filename)
     if service in passwords:
         return decrypt_message(passwords[service], key)
     return None
-
-# Delete a password
 def delete_password(filename, service):
     passwords = load_passwords(filename)
     if service in passwords:
         del passwords[service]
         save_passwords(filename, passwords)
 
-# Main program
 def main():
     master_password = input("Enter the master password: ")
     key = generate_key(master_password)
