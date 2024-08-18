4156008e09d39a41d23c77b14dd1e92336e0e7ba
Naveen06-dev <naveenkvg8@gmail.com>
2024-08-18 16:04:07 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,10 +1,8 @@
 import random
 
 def generate_otp():
-    # Generate a random 6-digit number
     otp = random.randint(100000, 999999)
     return otp
 
-# Example usage
 otp = generate_otp()
 print(f"Your OTP is: {otp}")
