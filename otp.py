e249d31305ea94979e573cdeb2a3edb1503d4b90
Naveen06-dev <naveenkvg8@gmail.com>
2024-08-18 17:08:40 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -6,3 +6,8 @@ def generate_otp():
 
 otp = generate_otp()
 print(f"Your OTP is: {otp}")
+a=int(input("Enter the OTP:"))
+if otp==a:
+    print("The OTP is correct")
+else:
+    print("Incorrect OTP")
